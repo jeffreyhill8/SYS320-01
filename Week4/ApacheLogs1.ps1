966ba54e0e4c01cc0eb15c9b874107bfe5136547
Jeffrey <jeffrey.hill@mymail.champlain.edu>
2026-02-04 15:04:32 -0500

    Week4 Scripts

---
+++ Week4/ApacheLogs1.ps1
@@ -0,0 +1,22 @@
+﻿function ApacheLogs1 {
+  $logsNotFormatted = Get-Content "C:\xampp\apache\logs\access.log"
+  $tableRecords = @()
+
+  foreach($line in $logsNotFormatted){
+    $section = $line -split ' '
+
+    $obtain = [PSCustomObject]@{
+      IP = $section[0]
+      Time = $section[3] + " " + $section[4]
+      Method = $section[5].Trim('"')
+      Page = $section[6]
+      Protocol = $section[7].Trim('"')
+      Response = $section[8]
+      Referrer = $section[10]
+    }
+
+    $tableRecords += $obtain
+  }
+
+  return $tableRecords
+}
---
+++ Week4/main.ps1
@@ -0,0 +1,6 @@
+﻿. .\ApacheLogs1.ps1
+
+$tableRecords = ApacheLogs1
+$tableRecords | Where-Object{ 
+    ($_.Page -like "*page2.html*") -and ($_.Referrer -like "*index.html*")
+} | Format-Table -AutoSize
