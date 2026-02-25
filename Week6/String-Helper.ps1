85ed8955dc2ce0b971da6ff8261f78236e7a0d83
Jeffrey <jeffrey.hill@mymail.champlain.edu>
2026-02-25 10:41:43 -0500

    Commits for week6

---
+++ Week6/String-Helper.ps1
@@ -0,0 +1,52 @@
+﻿<# String-Helper
+*************************************************************
+   This script contains functions that help with String/Match/Search
+   operations. 
+************************************************************* 
+#>
+
+
+<# ******************************************************
+   Functions: Get Matching Lines
+   Input:   1) Text with multiple lines  
+            2) Keyword
+   Output:  1) Array of lines that contain the keyword
+********************************************************* #>
+function getMatchingLines($contents, $lookline){
+
+$allines = @()
+$splitted =  $contents.split([Environment]::NewLine)
+
+for($j=0; $j -lt $splitted.Count; $j++){  
+ 
+   if($splitted[$j].Length -gt 0){  
+        if($splitted[$j] -ilike $lookline){ $allines += $splitted[$j] }
+   }
+
+}
+
+return $allines
+}
+
+function checkPassword($password) {
+    $plain = [Runtime.InteropServices.Marshal]::PtrToStringAuto(
+        [Runtime.InteropServices.Marshal]::SecureStringToBSTR($password))
+
+    if ($plain.Length -lt 6) {
+        return $false
+    }
+
+    if ($plain -notmatch '[^a-zA-Z0-]') {
+        return $false
+    }
+
+    if ($plain -notmatch '[0-9]') {
+        return $false
+    }
+
+    if ($plain -notmatch '[a-zA-Z]') {
+        return $false
+    }
+
+    return $true
+}
